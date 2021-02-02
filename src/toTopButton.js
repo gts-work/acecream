d956c43acbde5b7da49499c89a5ea3e685e1887f
Grygorii Tsaryk <71988119+gts-work@users.noreply.github.com>
2021-02-02 03:49:32 +0200

    updated top script scroll (#38)
    Co-authored-by: Grygorii Tsaryk <artgrem@gmail.com>

--- src/toTopButton.js
+++ src/toTopButton.js
@@ -1,23 +1,11 @@
 $(function () {
-
-        $(window).scroll(function () {
-
-          if ($(this).scrollTop() != 0) {
-
-            $('#toTop').fadeIn();
-
-          } else {
-
-            $('#toTop').fadeOut();
-
-          }
-
-        });
-
-        $('#toTop').click(function () {
-
-          $('body,html').animate({ scrollTop: 0 }, 800);
-
-        });
-
-      });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 600) {
+      $('#toTop').fadeIn();
+    } else {
+      $('#toTop').fadeOut();}
+  });
+  $('#toTop').click(function () {
+    $('body,html').animate({ scrollTop: 0 }, 800);
+  });
+});
