3b3cff3c5eef2e29f3b0f41cfe59591026e1a121
Grygorii Tsaryk <artgrem@gmail.com>
2021-02-02 17:52:29 +0200

    adde title icon - .ico and updated croll to top

--- src/toTopButton.js
+++ src/toTopButton.js
@@ -1,11 +1,29 @@
-$(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 600) {
-      $('#toTop').fadeIn();
-    } else {
-      $('#toTop').fadeOut();}
-  });
-  $('#toTop').click(function () {
-    $('body,html').animate({ scrollTop: 0 }, 800);
-  });
-});
+/* begin begin Back to Top button  */
+(function() {
+  'use strict';
+
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      goTopBtn.classList.add('products__go_top-show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('products__go_top-show');
+    }
+  }
+
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(-100000, -200000);
+      setTimeout(backToTop, 700);
+    }
+  }
+
+  var goTopBtn = document.querySelector('.products__go_top');
+
+  window.addEventListener('scroll', trackScroll);
+  goTopBtn.addEventListener('click', backToTop);
+})();
+/* end begin Back to Top button  */
